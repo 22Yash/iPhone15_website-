de3a517a035c3da8aa11e3a53727772c91983778
22Yash <yash.223234108@vcet.edu.in>
2024-04-30 16:40:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import Hero from './components/Hero'
+import Navabr from './components/Navabr'
+import Highlights from './components/Highlights'
+
+function App() {
+  
+
+  return (
+   <main className='bg-black '>
+   <Navabr/>
+   <Hero/>
+   <Highlights/>
+
+   
+
+
+   </main>
+  )
+}
+
+export default App
