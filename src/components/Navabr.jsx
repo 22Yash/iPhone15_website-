7fd2845a068e7862b77a954a955632f22994b0e6
22Yash <yash.223234108@vcet.edu.in>
2024-04-30 17:32:29 +0530

    done with hero section

--- src/components/Navabr.jsx
+++ src/components/Navabr.jsx
@@ -10,7 +10,7 @@ function Navabr() {
       <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt="Apple" width={14} height={14}/>
          <div className='flex flex-1 justify-center max-sm:hidden'>{navLists.map((nav,i) => (
-          <div className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
+          <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
           {nav}
         </div>
          )
