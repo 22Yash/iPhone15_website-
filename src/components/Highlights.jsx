ac6bc070794431561e2b18af3a49e03088e8b128
22Yash <yash.223234108@vcet.edu.in>
2024-04-30 18:13:59 +0530

    start with highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,17 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap'
 
 function Highlights() {
+  useGSAP(() => {
+  gsap.to('#title')
+  },[])
   return (
-    <div>
-      
-    </div>
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+     <div>
+      <h1 id='title' className='section-heading'></h1>
+     </div>
+    </section>
   )
 }
 
